818faa6abc76ff9032d61fc3ed37112ea6fe0166
rider <jil.p@simformsolutions.com>
2023-05-05 10:00:23 +0530

    add: ExceptionHandling class

---
+++ CSharp_practical_5/Program.cs
@@ -0,0 +1,24 @@
+﻿class ExceptionHandling
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("This is Finally Block");
+        }
+
+        Console.ReadKey();
+    }
+}
